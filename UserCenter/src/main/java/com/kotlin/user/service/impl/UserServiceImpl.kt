2e43aee85691b62ff05fa3cd51ea813ed2eb4561
lwb <18614005205@163.com>
2019-08-26 23:06:18 +0800

    完善retrofit网络配置 导入dagger2依赖包

--- UserCenter/src/main/java/com/kotlin/user/service/impl/UserServiceImpl.kt
+++ UserCenter/src/main/java/com/kotlin/user/service/impl/UserServiceImpl.kt
@@ -1,10 +1,26 @@
 package com.kotlin.user.service.impl
 
+import com.kotlin.base.data.protocol.BaseResq
+import com.kotlin.base.rx.BaseException
+import com.kotlin.user.data.UserRepository
 import com.kotlin.user.service.UserService
 import rx.Observable
+import rx.functions.Func1
 
 class UserServiceImpl : UserService {
     override fun rigister(mobile: String, verifyCode: String, pwd: String): Observable<Boolean> {
-        return Observable.just(true)
+        val repository = UserRepository()
+//        return repository.register(mobile, pwd, verifyCode).flatMap(Func1<BaseResq<String>, Observable<Boolean>> {
+//            Observable.just(true)
+//        })
+
+        return repository.register(mobile, pwd, verifyCode).flatMap(object : Func1<BaseResq<String>, Observable<Boolean>> {
+            override fun call(t: BaseResq<String>): Observable<Boolean> {
+                if (t.status == 0) {
+                    return Observable.error(BaseException(t.status, t.message))
+                }
+                return Observable.just(true)
+            }
+        })
     }
 }
